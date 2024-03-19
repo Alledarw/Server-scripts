fb2bbddd83a4be906dc7ac6a13e7c91553b1c7c9
Alexander Darwiche <82101046+Alledarw@users.noreply.github.com>
2024-03-19 12:27:09 +0000

    Added nginx option

--- switch-service.sh
+++ switch-service.sh
@@ -20,13 +20,25 @@ start_nodejs() {
     pm2 start all
 }
 
+# Function to stop nginx
+stop_nginx() {
+    service nginx stop
+}
+
+# Function to start nginx
+start_nginx() {
+    service nginx start
+}
+
+
 # Help function
 show_help() {
     echo "Usage: $0 [option]"
     echo "Options:"
     echo "  --help          Show this help message"
-    echo "  node            Start Node.js and stop Apache2"
-    echo "  apache          Start Apache2 and stop Node.js"
+    echo "  node            Start Node.js and stop Apache2, nginx"
+    echo "  apache          Start Apache2 and stop Node.js, nginx"
+    echo "  nginx           Start nginx and stop Node.js, Apache2"
 }
 
 # Main script
@@ -35,12 +47,20 @@ if [[ $# -eq 0 ]] || [[ $1 == "--help" ]]; then
     exit 0
 elif [[ $1 == "node" ]]; then
     stop_apache2
+    stop_nginx
     start_nodejs
     echo "Node.js is now running."
 elif [[ $1 == "apache" ]]; then
     stop_nodejs
+    stop_nginx
     start_apache2
     echo "Apache2 is now running."
+elif [[ $1 == "nginx" ]]; then
+    stop_nodejs
+    stop_apache2
+    start_nginx
+    echo "nginx is now running."
+
 else
     echo "Invalid option: $1"
     show_help
